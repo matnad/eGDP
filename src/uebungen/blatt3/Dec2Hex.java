2195c43be3775004dc84bf03c0df6bedf91a67cd
matnad <matnad@gmail.com>
2018-10-08 16:05:01 +0200

    blatt3

--- src/uebungen/blatt3/Dec2Hex.java
+++ src/uebungen/blatt3/Dec2Hex.java
@@ -4,13 +4,16 @@ public class Dec2Hex {
 	/*
 	 * Convert a decimal number to the hex format
 	 */
+
+	private static final String[] dec_2_hex = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.out.println("Bitte rufen Sie das Programm mit einem Eingabewert auf.");
 			System.exit(-1);
 		}
 		int dec = tryParse(args[0]);
-		//dec = 431;
+		//int dec = 87894745;
 		String hex = dec2hex(dec);
 
 		System.out.println(hex);
@@ -33,25 +36,13 @@ public class Dec2Hex {
 		}
 		dec = Math.abs(dec); // Get absolute value
 
-		// Use do...while to make it work with dec = 0 
+		// Use do...while to make it work with dec = 0
+		String hex_str_digit;
 		do {
 			int hex_digit = dec % 16;
 			dec /= 16;
-			switch (hex_digit) {
-			case 10: 	hex.insert(insert_position, "A");
-			break;
-			case 11: 	hex.insert(insert_position, "B"); 
-			break;
-			case 12: 	hex.insert(insert_position, "C");
-			break;
-			case 13: 	hex.insert(insert_position, "D"); 
-			break;
-			case 14: 	hex.insert(insert_position, "E"); 
-			break;
-			case 15: 	hex.insert(insert_position, "F");
-			break;
-			default:	hex.insert(insert_position, hex_digit);
-			}
+			hex_str_digit = dec_2_hex[hex_digit];
+			hex.insert(insert_position, hex_str_digit);
 		} while(dec > 0);
 
 		// Return hex string
@@ -68,5 +59,45 @@ public class Dec2Hex {
 			throw new java.lang.Error("Could not convert input to integer.");
 		}
 	}
+
+//	public static String dec2hex_switch(int dec) {
+//		/**
+//		 * This is ~30% slower than the array version
+//		 */
+//		// Initialize StringBuilder
+//		StringBuilder hex = new StringBuilder("0x");
+//		int insert_position = 2;
+//
+//		// Add a "-" for negative numbers and then work with the absolute value
+//		if (dec < 0) {
+//			hex.insert(0, "-");
+//			insert_position = 3;
+//		}
+//		dec = Math.abs(dec); // Get absolute value
+//
+//		// Use do...while to make it work with dec = 0
+//		do {
+//			int hex_digit = dec % 16;
+//			dec /= 16;
+//			switch (hex_digit) {
+//				case 10: 	hex.insert(insert_position, "A");
+//					break;
+//				case 11: 	hex.insert(insert_position, "B");
+//					break;
+//				case 12: 	hex.insert(insert_position, "C");
+//					break;
+//				case 13: 	hex.insert(insert_position, "D");
+//					break;
+//				case 14: 	hex.insert(insert_position, "E");
+//					break;
+//				case 15: 	hex.insert(insert_position, "F");
+//					break;
+//				default:	hex.insert(insert_position, hex_digit);
+//			}
+//		} while(dec > 0);
+//
+//		// Return hex string
+//		return hex.toString();
+//	}
 
 }
